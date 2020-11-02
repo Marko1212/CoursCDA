4b010ca92364398344500017706c1c942e9a3b44
Marko1212 <shihonage7@gmail.com>
2020-11-02 16:26:46 +0100

    update

--- javascript/10-jQuery/script.js
+++ javascript/10-jQuery/script.js
@@ -6,6 +6,14 @@ Faire un menu déroulant: au clic sur un bouton, un bloc de texte se déplie.Et 
 
 Créer un texte qui s'agrandit au clic. Si on reclique dessus, il reprend sa taille d'origine. Pour pouvoir faire cela, il va falloir mettre la taille de police initiale dans une variable.
 
+Créér 5 boutons et un rectangle. Chaque bouton provoque une action sur le rectangle.
+
+Bouton 1 : augmente la hauteur de 10px, si il dépasse 100px, il remet la hauteur à 10px
+(fonction height())
+Bouton 2 : mettre le rectangle en vert
+Bouton 3 : remettre les couleurs initiales
+Bouton 4 : faire disparaître le rectangle
+Bouton 5 : faire réaparaître le rectangle
  
 */
 
@@ -31,6 +39,34 @@ $(function () {
             'font-size', tailleDuTexte === "16px" ? "50px" : "16px");
     });
 
+    $("#button-1").on('click', function () {
+        var height = parseInt($(".rectangle").css('height'));
+        console.log(height);
+        if (height < 100) {
+            $(".rectangle").height("+=10");
+            console.log($(".rectangle").css('height'));
+        } else {
+            $(".rectangle").css(
+                'height', "10");
+        }
+
+    });
+
+    $("#button-2").on('click', function () {
+        $(".rectangle").css("backgroundColor", "green");
+    });
+
+    $("#button-3").on('click', function () {
+        $(".rectangle").css("backgroundColor", "red");
+    });
+
+    $("#button-4").on('click', function () {
+        $(".rectangle").hide();
+    });
+
+    $("#button-5").on('click', function () {
+        $(".rectangle").show();
+    });
 
 
 });
